3fb8f8462939f4a7dc15c311d0502ae5576ad7e4
confluence20 <t.beverages@gmail.com>
2023-09-16 01:50:17 +0100

    addition to my map

--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([6.511898984170893,3.3761417118579646], 17);
+var map = L.map('map').setView([7.824884142058772,6.691274959868235], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([6.511898984170893,3.3761417118579646]).addTo(map)
-		.bindPopup('The Technology Sandbox<br> Where Yoh is sitting this very moment')
+var marker = L.marker([7.824884142058772,6.691274959868235]).addTo(map)
+		.bindPopup('The Central Bank <br> Lokoja Branch')
 		.openPopup();	
